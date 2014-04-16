725ae0fee2dcca5b575c10a17fd8beedd92fa9e9
Atamert Ölçgen <muhuk@muhuk.com>
2014-04-16 20:49:07 +0000

    Updated project metadata.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clecs "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Entity-component-system for Clojure."
+  :url "https://github.com/muhuk/clecs"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
