c25c5a33c7c7ddd65449018ad4ad218d8bd0dd2a
Atamert Ölçgen <muhuk@muhuk.com>
2014-05-31 15:17:23 +0000

    Changed license to GPL v3.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject clecs "0.2.1-SNAPSHOT"
   :description "Entity-component-system for Clojure."
   :url "https://github.com/muhuk/clecs"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "GNU GPL v3"
+            :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :plugins [[jonase/eastwood "0.1.2"]
             [lein-cloverage "1.0.2"]
