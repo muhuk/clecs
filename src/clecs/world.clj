05ddbba48a906ee823ace55e11f23e30e6701c53
Atamert Ölçgen <muhuk@muhuk.com>
2014-06-29 14:57:28 +0000

    Added documentation for IEditableWorld functions.

--- src/clecs/world.clj
+++ src/clecs/world.clj
@@ -102,10 +102,49 @@
 
 
 (defprotocol IEditableWorld
-  (add-entity [this])
-  (remove-component [this eid ctype])
-  (remove-entity [this eid])
-  (set-component [this c]))
+  (add-entity
+   [this]
+   "Create a new entity in the world and return its id.")
+  (remove-component
+   [this eid ctype]
+   "Remove the component of type ``ctype`` that is associated
+   with ``eid`` and return ``nil``.
+
+   This method is a no-op if there is no relevant component.
+
+   #### Parameters:
+
+   eid
+   :   Entity id.
+
+   ctype
+   :   Component type. If you have a reference to a
+   ``component`` instance, use ``(type component)``.")
+  (remove-entity
+   [this eid]
+   "Delete the entity with ``eid``, all components
+   associated with it and return ``nil``.
+
+   This method is a no-op if there is no relevant entity.
+
+   #### Parameters:
+
+   eid
+   :   Entity id.")
+  (set-component
+   [this c]
+   "Add component ``c`` to the world and return ``nil``.
+
+   Entity id is already associated with ``c`` as a
+   consequence of ``IComponent`` protocol. If the entity
+   already has a component with the same type it will
+   be replaced.
+
+   #### Parameters:
+
+   c
+   :   Component instance. Must implement
+       [[clecs.component/IComponent]]."))
 
 
 (defprotocol IQueryableWorld
