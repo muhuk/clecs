7bbbed9bf26659834626624b09b19feea1c0a802
Atamert Ölçgen <muhuk@muhuk.com>
2014-09-06 12:43:02 +0000

    Incremented version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clecs "0.2.2-SNAPSHOT"
+(defproject clecs "0.2.3-SNAPSHOT"
   :description "Entity-component-system for Clojure."
   :url "https://github.com/muhuk/clecs"
   :license {:name "GNU GPL v3"
