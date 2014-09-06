127d0c822dcb84790afa8612c625591869b61162
Atamert Ölçgen <muhuk@muhuk.com>
2014-09-06 13:50:27 +0000

    Use Clojure 1.7.0a but test also with 1.6.0.

--- project.clj
+++ project.clj
@@ -3,20 +3,22 @@
   :url "https://github.com/muhuk/clecs"
   :license {:name "GNU GPL v3"
             :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0-alpha2"]]
   :plugins [[codox "0.8.8"]
             [jonase/eastwood "0.1.4"]
             [lein-cloverage "1.0.2"]
             [lein-midje "3.1.3"]]
   :profiles {:dev {:source-paths ["check"]
                    :dependencies [[midje "1.6.3"]
-                                  [org.clojure/test.check "0.5.9"]]}}
+                                  [org.clojure/test.check "0.5.9"]]}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}}
   :codox {:defaults {:doc/format :markdown}
           :src-dir-uri "http://github.com/muhuk/clecs/blob/master/"
           :src-linenum-anchor-prefix "L"
           :output-dir "target/doc/api"}
   :deploy-repositories {"releases" :clojars}
-  :aliases {"quickcheck"
+  :aliases {"all" ["with-profile" "dev:dev,1.6"]
+            "quickcheck"
             ^{:doc (str "Run quick-check to compare a backend against clecs.backend.atom-world\n"
                         "\n"
                         "    lein quickcheck $PATH.TO.WORLD/INITIALIZER $SAMPLES")}
