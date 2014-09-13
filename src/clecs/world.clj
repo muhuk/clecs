650657d7866289aee79e1832a96854a8b8e10703
Atamert Ölçgen <muhuk@muhuk.com>
2014-09-13 12:58:41 +0000

    Added documentation for ISystemManager & ITransactableWorld.

--- src/clecs/world.clj
+++ src/clecs/world.clj
@@ -175,11 +175,57 @@
 
 
 (defprotocol ISystemManager
-  (process! [this dt])
-  (remove-system! [this slabel])
-  (set-system! [this slabel s])
-  (systems [this]))
+  (process!
+   [this dt]
+   "Run systems using ``dt`` as time increment.
+
+   This is the function that will be called in
+   the main loop.
+
+   #### Parameters:
+
+   dt
+   :   Time passed since process! was called last
+       time. This value is passed to the systems.
+       It is recommended to use miliseconds as
+       unit.")
+  (remove-system!
+   [this slabel]
+   "Remove system registered with ``slabel``.
+
+   #### Parameters:
+
+   slabel
+   :   Label for the system to remove.")
+  (set-system!
+   [this slabel s]
+   "Register system ``s`` with label ``slabel``.
+
+   The system will not be run until ``process!``
+   is called.
+
+   #### Parameters:
+
+   slabel
+   :   A keyword to refer the system later.
+
+   s
+   :   System to register.")
+  (systems
+   [this]
+   "Return a sequence of all registered systems."))
 
 
 (defprotocol ITransactableWorld
-  (transaction! [this f]))
+  (transaction!
+   [this f]
+   "Execute the transaction ``f``.
+
+   The transaction may be retried. Side effects within
+   a transaction should be carefully considered.
+
+   #### Parameters:
+
+   f
+   :   A function that takes single ``ITransactableWorld``
+       parameter."))
