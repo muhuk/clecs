03b131d5a6df660dd6739403a4f307b0f01306c3
Atamert Ölçgen <muhuk@muhuk.com>
2014-09-13 13:33:43 +0000

    Implemented query primitives all & any.

---
+++ src/clecs/query.clj
@@ -0,0 +1,66 @@
+(ns clecs.query
+  "Primitives for query criteria.
+
+  `query` method of [[clecs.world/IQueryableWorld]]
+  is used to find entities based on their components.
+
+  #### Examples:
+
+      ;; Return a seq containing entity id's that
+      ;; has FooComponent:
+      (query queryable-world (all FooComponent))
+
+      ;; Same as the previous one:
+      (query queryable-world (any FooComponent))
+
+      ;; Entities with both FooComponent and BarComponent
+      (query queryable-world
+             (all FooComponent BarComponent))
+
+      ;; Entities with either FooComponent or BarComponent
+      (query queryable-world
+             (any FooComponent BarComponent))
+
+      ;; Entities with FooComponent and either
+      ;; BazComponent or BatComponent
+      (query queryable-world
+             (all FooComponent (any BazComponent
+                                    BatComponent)))
+
+      ;; Entities with either FooComponent and
+      ;; BarComponent or BazComponent and BatComponent
+      (query queryable-world
+             (any (all FooComponent BarComponent)
+                  (all BazComponent BatComponent)))
+
+  You can nest primitive calls infinitely. However
+  it is considered bad design to query too many
+  components at once.
+
+  See also [[clecs.world/IQueryableWorld]]."
+  (:require [clecs.component :refer [component-label
+                                     component-type?]]))
+
+
+(declare process-element)
+
+
+(defmacro ^:private make-query-command [command-symbol]
+  `(defn ~command-symbol
+     [& ~'elements]
+     (into [~(keyword command-symbol)]
+           (mapcat (process-element ~(keyword command-symbol)) ~'elements))))
+
+
+(defn ^:private process-element [same-keyword]
+  (fn [x]
+    (cond
+     (component-type? x) [(component-label x)]
+     (= (first x) same-keyword) (rest x)
+     :else [x])))
+
+
+(make-query-command all)
+
+
+(make-query-command any)
---
+++ test/clecs/query_test.clj
@@ -0,0 +1,75 @@
+(ns clecs.query-test
+  (:require [clecs.component :refer [component-label
+                                     component-type?]]
+            [clecs.query :refer :all]
+            [midje.sweet :refer :all]))
+
+
+(facts "all returns a vector with :all as its first parameter."
+       (all ..A..) => (has-prefix [:all])
+       (all ..A.. ..B.. ..C..) => (has-prefix [:all]))
+
+
+(fact "all converts component types to component labels."
+      (all ..ctype-1.. ..ctype-2..) => [:all ..clabel-1.. ..clabel-2..]
+      (provided (component-label ..ctype-1..) => ..clabel-1..
+                (component-label ..ctype-2..) => ..clabel-2..
+                (component-type? (as-checker anything)) => true))
+
+
+(fact "all inlines other all calls."
+      (all ..A.. (all ..B.. ..C..) ..D..) => [:all ..a.. ..b.. ..c.. ..d..]
+      (provided (component-label ..A..) => ..a..
+                (component-label ..B..) => ..b..
+                (component-label ..C..) => ..c..
+                (component-label ..D..) => ..d..
+                (component-type? ..A..) => true
+                (component-type? ..C..) => true
+                (component-type? ..B..) => true
+                (component-type? ..D..) => true
+                (component-type? [:all ..b.. ..c..]) => false))
+
+
+(fact "all doesn't modify any calls."
+      (all ..A.. (any ..B.. ..C..) ..D..) => [:all ..a.. ..any-result.. ..d..]
+      (provided (component-label ..A..) => ..a..
+                (component-label ..D..) => ..d..
+                (component-type? ..A..) => true
+                (component-type? ..D..) => true
+                (component-type? ..any-result..) => false
+                (any ..B.. ..C..) => ..any-result..))
+
+
+(facts "any returns a vector with :any as its first parameter."
+       (any ..A..) => (has-prefix [:any])
+       (any ..A.. ..B.. ..C..) => (has-prefix [:any]))
+
+
+(fact "any converts component types to component labels."
+      (any ..ctype-1.. ..ctype-2..) => [:any ..clabel-1.. ..clabel-2..]
+      (provided (component-label ..ctype-1..) => ..clabel-1..
+                (component-label ..ctype-2..) => ..clabel-2..
+                (component-type? (as-checker anything)) => true))
+
+
+(fact "any inlines other any calls."
+      (any ..A.. (any ..B.. ..C..) ..D..) => [:any ..a.. ..b.. ..c.. ..d..]
+      (provided (component-label ..A..) => ..a..
+                (component-label ..B..) => ..b..
+                (component-label ..C..) => ..c..
+                (component-label ..D..) => ..d..
+                (component-type? ..A..) => true
+                (component-type? ..C..) => true
+                (component-type? ..B..) => true
+                (component-type? ..D..) => true
+                (component-type? [:any ..b.. ..c..]) => false))
+
+
+(fact "any doesn't modify all calls."
+      (any ..A.. (all ..B.. ..C..) ..D..) => [:any ..a.. ..all-result.. ..d..]
+      (provided (component-label ..A..) => ..a..
+                (component-label ..D..) => ..d..
+                (component-type? ..A..) => true
+                (component-type? ..D..) => true
+                (component-type? ..all-result..) => false
+                (all ..B.. ..C..) => ..all-result..))
