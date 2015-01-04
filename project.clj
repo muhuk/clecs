c580d43ae84d4ac4192e34e182909d5021661206
Atamert Ölçgen <muhuk@muhuk.com>
2015-01-04 17:49:35 +0200

    Upgraded dependencies & incremented version.

--- project.clj
+++ project.clj
@@ -1,16 +1,16 @@
-(defproject clecs "1.0.3-SNAPSHOT"
+(defproject clecs "1.1.0-SNAPSHOT"
   :description "Entity-component-system for Clojure."
   :url "https://github.com/muhuk/clecs"
   :license {:name "GNU GPL v3"
             :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
   :dependencies [[org.clojure/clojure "1.7.0-alpha2"]]
   :plugins [[codox "0.8.10"]
-            [jonase/eastwood "0.1.5"]
+            [jonase/eastwood "0.2.1"]
             [lein-cloverage "1.0.2"]
             [lein-midje "3.1.3"]]
   :profiles {:dev {:source-paths ["check"]
                    :dependencies [[midje "1.6.3"]
-                                  [org.clojure/test.check "0.5.9"]]}
+                                  [org.clojure/test.check "0.6.2"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}}
   :codox {:defaults {:doc/format :markdown}
           :src-dir-uri "http://github.com/muhuk/clecs/blob/master/"
